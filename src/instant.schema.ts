61894d7f7eb61abacb46c497f1c85b17d169a466
stopachka <stepan.p@gmail.com>
2025-09-07 14:04:10 -0700

    roar

--- src/instant.schema.ts
+++ src/instant.schema.ts
@@ -1,27 +1,50 @@
 import { i } from "@instantdb/react";
 
-const graph = i.graph(
-  {
+const _schema = i.schema({
+  entities: {
+    $files: i.entity({
+      path: i.string().unique().indexed(),
+      url: i.string(),
+    }),
     $users: i.entity({
-      email: i.string().unique().indexed(),
+      email: i.string().unique().indexed().optional(),
     }),
     postBodies: i.entity({
       markdown: i.string(),
     }),
     posts: i.entity({
       createdAt: i.number(),
+      isDraft: i.boolean().optional(),
       number: i.number(),
       title: i.string(),
       updatedAt: i.number(),
-      isDraft: i.boolean().optional(),
     }),
   },
-  {
+  links: {
     postsBody: {
-      forward: { on: "posts", has: "one", label: "body" },
-      reverse: { on: "postBodies", has: "one", label: "post" },
+      forward: {
+        on: "posts",
+        has: "one",
+        label: "body",
+      },
+      reverse: {
+        on: "postBodies",
+        has: "one",
+        label: "post",
+      },
     },
-  }
-);
+  },
+  rooms: {
+    pages: {
+      presence: i.entity({}),
+    },
+  },
+});
 
-export default graph;
+// This helps Typescript display nicer intellisense
+type _AppSchema = typeof _schema;
+interface AppSchema extends _AppSchema {}
+const schema: AppSchema = _schema;
+
+export type { AppSchema };
+export default schema;
