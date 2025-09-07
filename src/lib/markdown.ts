45e0b3a8331dd71f13161b2868b075433f5d2902
stopachka <stepan.p@gmail.com>
2025-09-07 14:55:08 -0700

    rose pine theme

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -3,10 +3,10 @@ import Prism from "prismjs";
 import "prismjs/components/prism-clojure";
 import "prismjs/components/prism-sql";
 import "prismjs/components/prism-markdown";
-import "prism-themes/themes/prism-one-dark.css";
+import "../app/prism-rose-pine.css";
 
-// --------- 
-// Footnotes 
+// ---------
+// Footnotes
 // Inspo: https://github.com/markedjs/marked/issues/1562#issuecomment-1213367729
 
 const footnoteMatch = /^\[\^([^\]]+)\]:\s*([\s\S]*?)(?=\n\[\^|\n\n|$)/gm;
@@ -30,19 +30,17 @@ const interpolateFootnotes = (text: string) => {
   });
 };
 
-
 function paragraphWithFootnotes(text: string) {
   return marked.Renderer.prototype.paragraph.apply(null, [
     interpolateReferences(interpolateFootnotes(text)),
   ]);
 }
 
+// ------------
+// Highlighting
 
-// ------------ 
-// Highlighting 
-
-function codeWithHighlighting(code: string, lang: string = 'plaintext') {
-  const language = Prism.languages[lang] ? lang : 'plaintext';
+function codeWithHighlighting(code: string, lang: string = "plaintext") {
+  const language = Prism.languages[lang] ? lang : "plaintext";
   return (
     '<div class="not-prose">' +
     `<pre class="text-sm rounded-md p-2 overflow-x-auto font-normal language-${language}">` +
@@ -54,20 +52,20 @@ function codeWithHighlighting(code: string, lang: string = 'plaintext') {
   );
 }
 
-// ---------- 
-// Image with captions 
+// ----------
+// Image with captions
 
 function imageWithCaptions(href: string, title: string | null, text: string) {
   return (
     '<div class=" my-2 space-y-4">' +
     `<img src="${href}" alt="${text}" />` +
-    (title ? `<div class='text-center italic text-sm'>${title}</div>` : '') +
+    (title ? `<div class='text-center italic text-sm'>${title}</div>` : "") +
     "</div>"
-  )
+  );
 }
 
 // --------
-// Render 
+// Render
 
 marked.use({
   renderer: {
